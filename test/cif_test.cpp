de38e1674d4dcacbb5757dc9268b080f7e97b2d3
Antonio <me@antoniomallia.it>
2019-06-24 07:43:29 -0400

    First commit

---
+++ test/cif_test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "proto/common-index-format.pb.h"
+
+
+using namespace std;
+int main(int argc, char const *argv[])
+{
+  cout << "Hello Proto!" << endl;
+  return 0;
+}
+
