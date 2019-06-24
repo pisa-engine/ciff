4d087be99ed9c6e0c24068b2947b681c749039cd
Antonio <me@antoniomallia.it>
2019-06-24 17:33:52 -0400

    Added build to gitignore

--- build/test/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/amallia/common-index-format/test
-# Build directory: /home/amallia/common-index-format/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(cif_test "cif_test")
