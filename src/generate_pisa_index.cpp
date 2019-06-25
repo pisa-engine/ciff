4721f309c2e243b930a89ef166abd4d9100bf069
Antonio <me@antoniomallia.it>
2019-06-25 13:59:37 -0400

    Added output basename

--- src/generate_pisa_index.cpp
+++ src/generate_pisa_index.cpp
@@ -1,9 +1,26 @@
+#include <fstream>
+
+
 #include "google/protobuf/io/coded_stream.h"
 #include <google/protobuf/io/zero_copy_stream.h>
 #include <google/protobuf/io/zero_copy_stream_impl.h>
 #include "common-index-format.pb.h"
+#include "CLI/CLI.hpp"
 
 
 int main(int argc, char const *argv[])
 {
+    std::string postings_filename;
+    std::string output_basename;
+    
+    CLI::App app{"generate_pisa_index - a tool for generating a PISA index from a common index format."};
+    app.add_option("-p,--postings", postings_filename, "Postings filename")->required();
+    app.add_option("-o,--output", output_basename, "Output basename")->required();
+    CLI11_PARSE(app, argc, argv);
+
+    std::ofstream dstream(output_basename + ".docs");
+    std::ofstream fstream(output_basename + ".freqs");
+    std::ofstream sstream(output_basename + ".sizes");
+
+
 }
