dc19e094014979d4cb2edbc7888def162c0e4407
JMMackenzie <joel.m.mackenzie@gmail.com>
2020-01-14 11:43:35 +1100

    Not elegant but seems to work...

--- src/generate_pisa_index.cpp
+++ src/generate_pisa_index.cpp
@@ -27,9 +27,6 @@ void dump_postings_list(const PostingsList& postings_list) {
     std::cerr << std::endl; 
 }
 
-
-
-
 template <typename T>
 std::ostream &write_sequence(std::ostream &os, gsl::span<T> sequence)
 {
@@ -39,6 +36,87 @@ std::ostream &write_sequence(std::ostream &os, gsl::span<T> sequence)
     return os;
 }
 
+
+struct inverted_index {
+
+  std::unordered_map<uint32_t, std::vector<uint32_t>> m_documents {};
+  std::unordered_map<uint32_t, std::vector<uint32_t>> m_frequencies {};
+  std::map<uint32_t, uint32_t> m_document_sizes {};
+  std::vector<std::string> m_plain_terms {};
+  
+
+  void add_postings_list(const PostingsList& postings_list, uint32_t term_id) {
+
+      if (term_id % 10000 == 0) {
+          std::cerr << "Processing list " << term_id << "..." << std::endl;
+      }
+
+      std::string term = postings_list.term();
+      m_plain_terms.emplace_back(term);
+
+      uint32_t doc_freq = postings_list.df();
+      // uint32_t collection_freq = postings_list.cf();
+
+      std::vector<uint32_t> documents;
+      std::vector<uint32_t> frequencies;
+      m_documents[term_id].reserve(doc_freq);  
+      m_frequencies[term_id].reserve(doc_freq);
+
+      uint32_t pl_size = postings_list.posting_size();
+
+      if (doc_freq != pl_size) {
+          std::cerr << "Error: Posting size is not equal to document freq."
+                    << std::endl;
+          exit(EXIT_FAILURE);
+      }
+
+      uint32_t prev_id = 0; 
+      for (int64_t i = 0; i < postings_list.posting_size(); ++i) {
+          const Posting& posting = postings_list.posting(i);
+          uint32_t doc_id = prev_id + posting.docid();
+          uint32_t term_freq = posting.tf();
+          m_documents[term_id].push_back(doc_id);
+          m_frequencies[term_id].push_back(term_freq);
+          m_document_sizes[doc_id] += term_freq;
+          prev_id = doc_id;
+      }
+   }
+};
+
+
+void write(std::string const &output_basename,
+           inverted_index const &index,
+           uint32_t term_count) {
+
+    std::ofstream dstream(output_basename + ".docs");
+    std::ofstream fstream(output_basename + ".freqs");
+    std::ofstream sstream(output_basename + ".sizes");
+    std::ofstream lexstream(output_basename + ".lexicon.plain");
+
+    uint32_t doc_count = index.m_document_sizes.size();
+    write_sequence(dstream, gsl::make_span<uint32_t const>(&doc_count, 1));
+    for (size_t term_id = 0; term_id < term_count; ++term_id) {
+        auto pos = index.m_documents.find(term_id);
+        if (pos != index.m_documents.end()) {
+            auto const &docs = pos->second;
+            auto const &freqs = index.m_frequencies.at(term_id);
+            write_sequence(dstream, gsl::span<uint32_t const>(docs));
+            write_sequence(fstream, gsl::span<uint32_t const>(freqs));
+        } else {
+            write_sequence(dstream, gsl::span<uint32_t const>());
+            write_sequence(fstream, gsl::span<uint32_t const>());
+        }
+        lexstream << index.m_plain_terms[term_id] << std::endl;
+    }
+
+    std::vector<uint32_t> doc_sizes;
+    std::transform(index.m_document_sizes.begin(), index.m_document_sizes.end(), 
+                   std::back_inserter(doc_sizes), 
+                   [](const auto &pair) {
+        return pair.second;
+    });
+    write_sequence(sstream, gsl::span<uint32_t const>(doc_sizes));
+}
 
 int main(int argc, char const *argv[])
 {
@@ -53,14 +131,13 @@ int main(int argc, char const *argv[])
     app.add_option("-o,--output", output_basename, "Output basename")->required();
     CLI11_PARSE(app, argc, argv);
 
-    std::ofstream dstream(output_basename + ".docs");
-    std::ofstream fstream(output_basename + ".freqs");
-    std::ofstream sstream(output_basename + ".sizes");
+    inverted_index invidx;
 
     std::ifstream postings_data(postings_filename, std::ios::binary);
     google::protobuf::io::ZeroCopyInputStream* postings_stream = new google::protobuf::io::IstreamInputStream(&postings_data);
     google::protobuf::io::CodedInputStream coded_stream(postings_stream);
 
+    uint32_t term_id = 0;
     while (true) {
         uint32_t message_size;
         if (!coded_stream.ReadVarint32(&message_size)) {
@@ -73,8 +150,12 @@ int main(int argc, char const *argv[])
             exit(EXIT_FAILURE);
         }
         coded_stream.PopLimit(size_limit);
-        dump_postings_list(postings_list);
+        invidx.add_postings_list(postings_list, term_id);
+        ++term_id;
     }
+  
+    std::cerr << "Writing canonical index..." << std::endl;
+    write(output_basename, invidx, term_id);
 
     delete postings_stream;
 }
