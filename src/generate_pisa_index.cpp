274ed882502e1b6aa73ee10ed758bf721faa5887
JMMackenzie <joel.m.mackenzie@gmail.com>
2020-01-20 11:30:20 +1100

    Refactor WRT Michal's comments

--- src/generate_pisa_index.cpp
+++ src/generate_pisa_index.cpp
@@ -11,25 +11,6 @@
 
 using namespace cif;
 
-void dump_postings_list(const PostingsList& postings_list) {
-    std::cerr << "Term = [" << postings_list.term() << "]" << std::endl;
-    std::cerr << "Document Frequency/Collection Term Frequency = [" 
-              << postings_list.df() << "," << postings_list.cf() << "]" 
-              << std::endl;
-   
-    int32_t prev_id = 0; 
-    for (int64_t i = 0; i < postings_list.posting_size(); ++i) {
-        const Posting& posting = postings_list.posting(i);
-        int32_t doc_id = prev_id + posting.docid();
-        std::cerr << "[" << doc_id << "," << posting.tf() << "] ";
-        prev_id = doc_id;
-    }
-    std::cerr << std::endl; 
-}
-
-void dump_term(const PostingsList& postings_list, uint32_t term_id) {
-  std::cerr << postings_list.term() << " " << term_id << " " << postings_list.df() << "\n";
-}
 
 template <typename T>
 std::ostream &write_sequence(std::ostream &os, gsl::span<T> sequence)
@@ -40,26 +21,27 @@ std::ostream &write_sequence(std::ostream &os, gsl::span<T> sequence)
     return os;
 }
 
-
 struct inverted_index {
 
-  std::unordered_map<uint32_t, std::vector<uint32_t>> m_documents {};
-  std::unordered_map<uint32_t, std::vector<uint32_t>> m_frequencies {};
-  std::map<uint32_t, uint32_t> m_document_sizes {};
+  std::vector<std::vector<uint32_t>> m_documents {};
+  std::vector<std::vector<uint32_t>> m_frequencies {};
+  std::vector<std::pair<uint32_t, uint32_t>> m_document_sizes {};
   std::vector<std::string> m_plain_terms {};
  
-  void add_document_lengths(const std::string doclen_file) {
+  void add_document_lengths(const std::string& doclen_file) {
       std::ifstream input_lengths(doclen_file);
       uint32_t doc_id;
       uint32_t size;
       while (input_lengths >> doc_id >> size) {
-          m_document_sizes[doc_id] = size;
+          m_document_sizes.emplace_back(doc_id, size);
       }  
       std::cerr << "Read " << m_document_sizes.size() << " document lengths."
                 << std::endl;
   } 
 
-  void add_postings_list(const PostingsList& postings_list, uint32_t term_id) {
+  void add_postings_list(const PostingsList& postings_list) {
+
+      auto term_id = static_cast<uint32_t>(m_documents.size());
 
       if (term_id % 10000 == 0) {
           std::cerr << "Processing list " << term_id << "..." << std::endl;
@@ -69,12 +51,11 @@ struct inverted_index {
       m_plain_terms.emplace_back(term);
 
       uint32_t doc_freq = postings_list.df();
-      // uint32_t collection_freq = postings_list.cf();
 
       std::vector<uint32_t> documents;
       std::vector<uint32_t> frequencies;
-      m_documents[term_id].reserve(doc_freq);  
-      m_frequencies[term_id].reserve(doc_freq);
+      documents.resize(doc_freq);
+      frequencies.resize(doc_freq);
 
       uint32_t pl_size = postings_list.posting_size();
 
@@ -89,17 +70,19 @@ struct inverted_index {
           const Posting& posting = postings_list.posting(i);
           uint32_t doc_id = prev_id + posting.docid();
           uint32_t term_freq = posting.tf();
-          m_documents[term_id].push_back(doc_id);
-          m_frequencies[term_id].push_back(term_freq);
+          documents[i] = doc_id;
+          frequencies[i] = term_freq;
           prev_id = doc_id;
       }
+  
+      m_documents.push_back(documents);
+      m_frequencies.push_back(frequencies);
    }
 };
 
 
 void write(std::string const &output_basename,
-           inverted_index const &index,
-           uint32_t term_count) {
+           inverted_index &index) {
 
     std::ofstream dstream(output_basename + ".docs");
     std::ofstream fstream(output_basename + ".freqs");
@@ -108,20 +91,15 @@ void write(std::string const &output_basename,
 
     uint32_t doc_count = index.m_document_sizes.size();
     write_sequence(dstream, gsl::make_span<uint32_t const>(&doc_count, 1));
-    for (size_t term_id = 0; term_id < term_count; ++term_id) {
-        auto pos = index.m_documents.find(term_id);
-        if (pos != index.m_documents.end()) {
-            auto const &docs = pos->second;
-            auto const &freqs = index.m_frequencies.at(term_id);
-            write_sequence(dstream, gsl::span<uint32_t const>(docs));
-            write_sequence(fstream, gsl::span<uint32_t const>(freqs));
-        } else {
-            write_sequence(dstream, gsl::span<uint32_t const>());
-            write_sequence(fstream, gsl::span<uint32_t const>());
-        }
+    for (size_t term_id = 0; term_id < index.m_documents.size(); ++term_id) {
+        auto const &docs = index.m_documents.at(term_id);
+        auto const &freqs = index.m_frequencies.at(term_id);
+        write_sequence(dstream, gsl::span<uint32_t const>(docs));
+        write_sequence(fstream, gsl::span<uint32_t const>(freqs));
         lexstream << index.m_plain_terms[term_id] << std::endl;
     }
 
+    std::sort(index.m_document_sizes.begin(), index.m_document_sizes.end());
     std::vector<uint32_t> doc_sizes;
     std::transform(index.m_document_sizes.begin(), index.m_document_sizes.end(), 
                    std::back_inserter(doc_sizes), 
@@ -155,7 +133,6 @@ int main(int argc, char const *argv[])
     google::protobuf::io::ZeroCopyInputStream* postings_stream = new google::protobuf::io::IstreamInputStream(&postings_data);
     google::protobuf::io::CodedInputStream coded_stream(postings_stream);
 
-    uint32_t term_id = 0;
     while (true) {
         uint32_t message_size;
         if (!coded_stream.ReadVarint32(&message_size)) {
@@ -168,11 +145,10 @@ int main(int argc, char const *argv[])
             exit(EXIT_FAILURE);
         }
         coded_stream.PopLimit(size_limit);
-        invidx.add_postings_list(postings_list, term_id);
-        ++term_id;
+        invidx.add_postings_list(postings_list);
     }
   
     std::cerr << "Writing canonical index..." << std::endl;
-    write(output_basename, invidx, term_id);
+    write(output_basename, invidx);
     delete postings_stream;
 }
