fdb85bda9a58613164d8b37c2e3d13c6a039d4a6
Michal Siedlaczek <michal.siedlaczek@nyu.edu>
2020-01-28 20:54:23 -0500

    Add rust implementation

---
+++ rust/build.rs
@@ -0,0 +1,13 @@
+extern crate protobuf_codegen_pure;
+
+fn main() {
+    protobuf_codegen_pure::run(protobuf_codegen_pure::Args {
+        out_dir: "src/proto",
+        input: &["proto/common-index-format.proto"],
+        includes: &["proto"],
+        customize: protobuf_codegen_pure::Customize {
+            ..Default::default()
+        },
+    })
+    .expect("protoc");
+}
